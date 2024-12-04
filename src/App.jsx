6b2e3c8c80b01e9fd77eedbcbdf759bb9442e18a
kamal kishore <kamal.kishore981@gmail.com>
2024-12-04 09:59:54 -0500

    added another component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Sidebar from "./components/Sidebar";
 import CreatePost from "./components/Createpost";
-import Post from "./components/Post";
+import PostList from "./components/PostList";
 function App() {
   return (
     <div className="app-container">
@@ -12,7 +12,7 @@ function App() {
       <div className="content">
         <Header></Header>
         <CreatePost></CreatePost>
-        <Post></Post>
+        <PostList></PostList>
         <Footer></Footer>
       </div>
     </div>
