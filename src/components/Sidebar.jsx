7f87c3683ad6d0a6eab8532dcbee387b79d2bafc
kamal kishore <kamal.kishore981@gmail.com>
2024-12-04 18:47:22 -0500

    add event listner to the home to show all the posts and add event listner to create post to show dummy form

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
-const Sidebar = () => {
+//we have received selectedTab and setSelectedTab as props .
+const Sidebar = ({ selectedTab, setSelectedTab }) => {
   return (
     <div
       className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark sidebar"
@@ -15,66 +16,46 @@ const Sidebar = () => {
       </a>
       <hr />
       <ul className="nav nav-pills flex-column mb-auto">
-        <li className="nav-item">
-          <a href="#" className="nav-link active" aria-current="page">
+        {/* here we have attached onClick event listener on the list item which will call setSelectedTab to set the value to Home*/}
+        <li
+          className="nav-item"
+          onClick={() => {
+            setSelectedTab("Home");
+          }}
+        >
+          <a
+            href="#"
+            className={`nav-link text-white ${
+              selectedTab === "Home" && "active"
+            }`}
+            aria-current="page"
+          >
             <svg className="bi pe-none me-2" width="16" height="16">
               <use xlinkHref="#home"></use>
             </svg>
             Home
           </a>
         </li>
-        <li>
-          <a href="#" className="nav-link text-white">
+        {/* here we have attached onClick event listener on the list item which will call setSelectedTab to set the value to Create Post*/}
+        <li
+          onClick={() => {
+            setSelectedTab("Create Post");
+          }}
+        >
+          <a
+            href="#"
+            className={`nav-link text-white ${
+              selectedTab === "Create Post" && "active"
+            }`}
+          >
             <svg className="bi pe-none me-2" width="16" height="16">
               <use xlinkHref="#speedometer2"></use>
             </svg>
-            Create post
+            Create Post
           </a>
         </li>
       </ul>
       <hr />
-      <div className="dropdown">
-        <a
-          href="#"
-          className="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
-        >
-          <img
-            src="https://github.com/mdo.png"
-            alt=""
-            width="32"
-            height="32"
-            className="rounded-circle me-2"
-          />
-          <strong>mdo</strong>
-        </a>
-        <ul className="dropdown-menu dropdown-menu-dark text-small shadow">
-          <li>
-            <a className="dropdown-item" href="#">
-              New project...
-            </a>
-          </li>
-          <li>
-            <a className="dropdown-item" href="#">
-              Settings
-            </a>
-          </li>
-          <li>
-            <a className="dropdown-item" href="#">
-              Profile
-            </a>
-          </li>
-          <li>
-            <hr className="dropdown-divider" />
-          </li>
-          <li>
-            <a className="dropdown-item" href="#">
-              Sign out
-            </a>
-          </li>
-        </ul>
-      </div>
     </div>
   );
 };
